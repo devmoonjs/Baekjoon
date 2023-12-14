0eef0690d167bedfe1104f9cfa71182fad4ade95
Moon <130039001+devmoonjs@users.noreply.github.com>
2023-12-14 11:32:14 +0900

    [Bronze V] Title: Hello World, Time: 112 ms, Memory: 13820 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,16 @@
+class print{
+	String a = "Hello World!";
+	
+	public void b(){
+		System.out.println(a);
+	}
+	
+}
+
+public class Main {
+	public static void main(String[] args) {
+		print c = new print();
+		c.b();
+	}
+
+}
