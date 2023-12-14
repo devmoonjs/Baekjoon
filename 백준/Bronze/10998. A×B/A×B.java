c1f99ae895a205db84a323e4e6626ea17d02213d
Moon <130039001+devmoonjs@users.noreply.github.com>
2023-12-14 11:37:35 +0900

    [Bronze V] Title: A×B, Time: 216 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class gop{
+	
+	Scanner scan = new Scanner(System.in);
+	int a = scan.nextInt();
+	int b = scan.nextInt();
+	
+	public void b(){
+		System.out.println(a*b);
+	}
+	
+}
+
+public class Main {
+	public static void main(String[] args) {
+		gop c = new gop();
+		c.b();
+	}
+
+}
