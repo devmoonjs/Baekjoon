f29bbe6079d06f0ef0899def99e870fc42465e0c
Moon <130039001+devmoonjs@users.noreply.github.com>
2023-12-17 23:59:34 +0900

    [Bronze V] Title: A+B - 7, Time: 260 ms, Memory: 18668 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class SumAB{
+	
+	Scanner scan = new Scanner(System.in);
+	
+	int T;
+	int A;
+	int B;
+	int sum;
+	
+	public int getT() {
+		T = scan.nextInt();
+		
+		return T;
+	}
+	
+	public void result() {
+		for(int i=1; i<T+1; i++) {
+			A = scan.nextInt();
+			B = scan.nextInt();
+			int R = A + B;
+			System.out.println("Case #"+ i +": " + R);
+		}
+	}
+	
+}
+
+public class Main {
+
+	public static void main(String[] args){
+		SumAB a = new SumAB();
+		a.getT();
+		a.result();
+			
+	}
+		
+}
