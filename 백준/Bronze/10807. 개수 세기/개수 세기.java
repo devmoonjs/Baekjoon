c89dc13dde82e95f125881163493325df73e8987
Moon <130039001+devmoonjs@users.noreply.github.com>
2023-12-19 21:09:40 +0900

    [Bronze V] Title: 개수 세기, Time: 216 ms, Memory: 17824 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,23 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
+
 public class Main {
-	static Scanner scan = new Scanner(System.in);
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scan = new Scanner(System.in);
 		
 		int n = scan.nextInt();
-		int array[] = new int[n];
-		int count = 0;
+		int[] arr = new int[n];
 		
 		for (int i = 0; i<n; i++) {
-			array[i] = scan.nextInt();
-		} 
-		int v = scan.nextInt();
+			arr[i] = scan.nextInt();
+		}
 		
-		for(int j = 0; j<n; j++) {
-			if (array[j] == v) {
+		int V = scan.nextInt();
+		int count = 0;
+		
+		for (int i = 0; i<arr.length; i++) {
+			if(V == arr[i]) {
 				count++;
 			}
 		}
 		System.out.println(count);
 	}
+		
 }
+
+
+
