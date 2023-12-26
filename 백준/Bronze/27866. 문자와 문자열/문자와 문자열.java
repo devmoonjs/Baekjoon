b1b3694a201adc75ed5edbb99305bd0e8ba8fe19
Moon <130039001+devmoonjs@users.noreply.github.com>
2023-12-26 18:30:00 +0900

    [Bronze V] Title: 문자와 문자열, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,12 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String S = scan.next();
-		int i = scan.nextInt();
-		String[] array = S.split("");
-		System.out.println(array[i-1]);
-		
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String Input = bf.readLine();
+        
+        int i = Integer.parseInt(bf.readLine());
+        
+        String X = Input.substring(i-1,i);
+        System.out.println(X);
+        
+    }
 }
