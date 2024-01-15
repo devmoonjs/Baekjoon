b4ec149549674eab5a2a2c3e02b2e343e69ed2ae
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-01-15 16:59:33 +0900

    [level Lv.2] Title: 전화번호 목록, Time: 311.86 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/Lv.2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/Lv.2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,15 +1,6 @@
-import java.util.Arrays;
-
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-
-        Arrays.sort(phone_book); // 배열 오름차순 정렬
-
-        for (int i =0; i< phone_book.length-1; i++){
-            if (phone_book[i+1].startsWith(phone_book[i]))
-                return false;
-        }
-        return true;
+        return answer;
     }
 }
