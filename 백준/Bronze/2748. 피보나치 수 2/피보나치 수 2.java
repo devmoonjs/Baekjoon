7ce619f97a147480dd9b1dfe8d55ce9343e6ef56
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-05-29 00:44:12 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static long[] f;
+	
+	public static void main (String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		f = new long[n+1];
+		
+		f[0] = 0;
+		f[1] = 1;
+		
+		for (int i = 2; i <= n; i++) {
+			f[i] = f[i-1] + f[i-2];
+		}
+		
+		System.out.println(f[n]);
+	}
+}
