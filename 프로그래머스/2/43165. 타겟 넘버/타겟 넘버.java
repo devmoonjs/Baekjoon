45e07c4d610c91cf260d56a520a448a1072626a8
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-06-07 12:51:14 +0900

    [level 2] Title: 타겟 넘버, Time: 0.32 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,49 +1,38 @@
+/*
+* 1. dfs(인덱스, 누적합계);
+*/
 import java.util.*;
 
 class Solution {
     
+    static int target;
+    static int len;
     static int answer;
     static int[] numbers;
-    static int target;
     
-    static class Point{
-        int index;
-        int sum;
+    private static void dfs(int index, int sum) {
         
-        Point(int index, int sum) {
-            this.index = index;
-            this.sum = sum;
+        if (index == len) {
+            if (sum == target) {
+                answer++;
+            }
         }
-    }    
-    
-    private static void bfs(int index, int sum) {
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(index, sum));
         
-        while(!q.isEmpty()){
-            Point temp = q.poll();
-            
-            // index가 numbers.length에 도달하면 검사
-            if (temp.index == numbers.length) {
-                if (temp.sum == target) {
-                    answer++;
-                }
-                // continue; // 더 이상 탐색하지 않도록 continue
-            }
-            
-            // index가 numbers.length를 넘지 않는 경우에만 큐에 추가
-            if (temp.index < numbers.length) {
-                q.add(new Point(temp.index + 1, temp.sum + numbers[temp.index]));
-                q.add(new Point(temp.index + 1, temp.sum - numbers[temp.index]));
-            }
+        if (index < len) {
+            dfs(index + 1, sum + numbers[index]);
+            dfs(index + 1, sum - numbers[index]);
         }
     }
     
     public int solution(int[] numbers, int target) {
+        
+        answer = 0;
         this.numbers = numbers;
         this.target = target;
-        answer = 0;
-        bfs(0, 0);
+        this.len = numbers.length;
+        
+        dfs(0,0);
+        
         return answer;
     }
 }
