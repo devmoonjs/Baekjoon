44a86c0d764e5c05e78725558a7875885724dc8d
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-06-14 18:28:17 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,12 @@
+-- 22년 10월 5일에 등록된 게시물의 정보 출력 -> 거래상태가 slase 이면 판매중, reserved면 예약중, done 이면 거래완료
+
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, 
+    CASE
+    WHEN STATUS = 'SALE' THEN '판매중'
+    WHEN STATUS = 'RESERVED' THEN '예약중'
+    WHEN STATUS = 'DONE' THEN '거래완료'
+    END AS STATUS
+FROM USED_GOODS_BOARD
+WHERE CREATED_DATE = '2022-10-05'
+ORDER BY BOARD_ID DESC;
+    
