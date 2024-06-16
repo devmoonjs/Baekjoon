759da7396f172f520bfadf31b95235266877b373
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-06-16 10:32:42 +0900

    [level 3] Title: 네트워크, Time: 0.09 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,24 +1,24 @@
 class Solution {
     
     static boolean[] visited;
-    static int n;
     static int[][] computers;
+    static int n;
     
-    private static void dfs(int index) {
+    private static void dfs(int index) { // index -> 노드 번호, 각 노드마다 자식 노드를 체크
         visited[index] = true;
         
-        for (int i = 0; i < n; i++ ) {
-            if (computers[index][i] != 0 & !visited[i]) {
+        for (int i = 0 ; i < n; i++) {
+            if (computers[index][i] != 0 && !visited[i]) {
                 dfs(i);
             }
         }
     }
     
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        this.n = n;
         this.computers = computers;
+        this.n = n;
         visited = new boolean[n];
+        int answer = 0;
         
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
