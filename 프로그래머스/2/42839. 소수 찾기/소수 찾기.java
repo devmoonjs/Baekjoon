0bbc487054135a360e242b116c1c8031afcf9077
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-06-17 17:34:29 +0900

    [level 2] Title: 소수 찾기, Time: 13.16 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    static HashSet<Integer> numbersSet = new HashSet<>();
+    HashSet<Integer> numbersSet = new HashSet<>();
     
     public void recursive(String comb, String others) {
         if (!comb.equals("")) {
@@ -13,37 +13,10 @@ class Solution {
                       others.substring(0,i) + others.substring(i + 1));
         }
     }
-    
-    private static boolean isPrime(int index) {
-		if (index == 1 || index == 0) {
-			return false;
-		}
-		
-		int lim = (int)Math.sqrt(index);
-		for (int i = 2; i <= lim; i++) {
-			if (index % i == 0) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
-	
     public int solution(String numbers) {
-        int count = 0;
+        int answer = 0;
         recursive("", numbers);
-   
-    	Iterator<Integer> it = numbersSet.iterator();
-    	
-		while(it.hasNext()) {
-			int number = it.next();
-			if (isPrime(number)) {
-				count++;
-			}
-		}
-        // System.out.println(numbersSet);
-    	
-    	return count;
+        System.out.println(numbersSet);
+        return answer;
     }
-
 }
