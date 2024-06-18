cd8984a2e4baebf8331c3f3a3f0b71a0c5c09ca7
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-06-18 13:17:40 +0900

    [level 2] Title: 소수 찾기, Time: 14.92 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,22 +1,51 @@
 import java.util.*;
 
 class Solution {
-    HashSet<Integer> numbersSet = new HashSet<>();
     
-    public void recursive(String comb, String others) {
-        if (!comb.equals("")) {
+    static HashSet<Integer> numbersSet = new HashSet<>();
+    
+    private static void recursive(String comb, String others) {
+        if (comb != "") {
             numbersSet.add(Integer.valueOf(comb));
         }
         
-        for (int i = 0; i < others.length(); i++) {
-            recursive(comb + others.charAt(i), 
-                      others.substring(0,i) + others.substring(i + 1));
+        for (int i = 0; i < others.length(); i++ ) {
+            recursive(comb + others.charAt(i), others.substring(0,i) + others.substring(i+1));
         }
     }
+    
+    private static boolean isPrime(int index) {
+        if (index == 1 || index == 0) {
+            return false;
+        }
+        
+        int lim = (int)Math.sqrt(index);
+        
+        for (int i = 2 ; i <= lim ; i++) {
+            if (index % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    
     public int solution(String numbers) {
-        int answer = 0;
+        
         recursive("", numbers);
-        System.out.println(numbersSet);
+        
+        Iterator<Integer> it = numbersSet.iterator();
+        
+        int answer = 0;
+        
+        while(it.hasNext()){
+            int temp = it.next();
+            boolean result = isPrime(temp);
+            
+            if (result) {
+                answer++;
+            }
+        }
         return answer;
     }
 }
