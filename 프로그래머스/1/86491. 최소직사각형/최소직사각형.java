db1947c825c996a3025df0e0ae5323177b3c8f09
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-06-19 12:27:19 +0900

    [level 1] Title: 최소직사각형, Time: 1.69 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,24 +1,16 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
     public int solution(int[][] sizes) {
-        ArrayList<Integer> aList = new ArrayList<>();
-        ArrayList<Integer> bList = new ArrayList<>();
+        int aMax = 0;
+        int bMax = 0;
         
-        for (int[] arr : sizes){
-            int a = Math.max(arr[0], arr[1]);
-            int b = Math.min(arr[0], arr[1]);
-            aList.add(a);
-            bList.add(b);
+        for (int[] s : sizes) {
+            int a = Math.max(s[0],s[1]);
+            int b = Math.min(s[0],s[1]);
+            
+            if (a > aMax) aMax = a;
+            if (b > bMax) bMax = b;
         }
         
-        Collections.sort(aList);
-        Collections.sort(bList);
-        
-        int aMax = aList.get(aList.size()-1);
-        int bMax = bList.get(bList.size()-1);
-       
         return aMax * bMax;
     }
 }
