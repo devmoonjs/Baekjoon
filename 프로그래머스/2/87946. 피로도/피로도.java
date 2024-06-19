0c70c0e3eb75fe93608c474a55cc4fafd15a08b3
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-06-19 15:29:50 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,40 +1,41 @@
 import java.util.*;
 
 class Solution {
+
+    static int len;
+    static int[][] d;
+    static int max = 0;
     
-    static int d;
-    static boolean[] visited;
-    static int[][] dungeons;
-    static int max;
-    
-    private static void dfs(int k, int cnt) {
+    private static void dfs(int k, boolean[] visited, int cnt) {
+        
         max = Math.max(max, cnt);
         
-        for (int i = 0; i < d; i++) {
-            if (!visited[i] && dungeons[i][0] <= k) {
-                visited[i] = true;
-                dfs(k - dungeons[i][1], cnt + 1);
-                visited[i] = false;
-            } 
+        for (int j = 0; j < len; j++) {
+            if (!visited[j]) {
+                if (k >= d[j][0]) {
+                    visited[j] = true;
+                    dfs(k - d[j][1], visited, cnt+1);
+                    visited[j] = false;
+                }
+            }
         }
     }
     
     public int solution(int k, int[][] dungeons) {
-        d = dungeons.length;
-        visited = new boolean[d];
-        Solution.dungeons = dungeons;
+        len = dungeons.length;
+        this.d = dungeons;
+        boolean visited[] = new boolean[len];
         
-        max = 0;
         
-        for (int i = 0; i < d; i ++) {
-            if (dungeons[i][0] <= k) {
+        for (int i = 0; i < len; i++) {
+            if (k >= d[i][0]) {
                 Arrays.fill(visited, false);
                 visited[i] = true;
-                dfs(k - dungeons[i][1], 1);
+                dfs(k-d[i][1], visited, 1);
+                visited[i] = false;
             }
         }
         
-       return max;
+        return max;
     }
 }
-
