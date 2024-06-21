55c0a2d83b19b84c5a4af9b161badf7dc06e40c7
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-06-21 11:57:35 +0900

    [level 2] Title: 올바른 괄호, Time: 24.24 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,33 +1,34 @@
-import java.util.Stack;
+// '('는 그냥 넣음
+// ')'는 이전 값 비교 -> '(' 이면 하나 빼기
+// 마지막에 스택 비었는지 확인
+import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-		Stack<String> stack = new Stack<>();
-		
-		for (int i = 0; i < s.length(); i++) {
-			String temp = String.valueOf(s.charAt(i));
-			
-			if (temp.equals("(")) {
-				stack.push(temp);
-			}
-			
-			if (temp.equals(")")) {
-				if (!stack.isEmpty()) {
-					stack.pop();
-				} else {
-					answer = false;
-					return answer;
-				}
-			}
-		}
-		
-		if (!stack.isEmpty()) {
-			answer = false;
-			return answer;
-		} else {
-			return answer;
-			
-		}
-	}
+        
+        boolean answer;
+        
+        Stack<String> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            String temp = String.valueOf(s.charAt(i));
+            
+            if (temp.equals("(")) {
+                stack.push(temp);
+            }
+            
+            else if (!stack.isEmpty()) {
+                stack.pop();
+            } else {
+                answer = false;
+                return answer;
+            }
+        }
+        
+        if (stack.isEmpty()) {
+            answer = true;
+        } else answer = false;
+        
+        return answer;
+    }
 }
