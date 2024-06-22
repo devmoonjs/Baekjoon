6df24cf79ab1601f36fd2e2a9d08c11f718f2fd8
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-06-22 20:51:59 +0900

    [level 2] Title: 소수 찾기, Time: 12.78 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,39 +1,38 @@
+// 8:35 ~ 8:55
+
 import java.util.*;
 
 class Solution {
     
-    static HashSet<Integer> numbersSet = new HashSet<>();
+    static HashSet<Integer> set = new HashSet<>();
     
-    private static void recursive(String comb, String others) {
-        
-        if (comb != "") numbersSet.add(Integer.valueOf(comb));
+    static private void recursive(String comb, String others) {
+        if (comb != "" ) set.add(Integer.valueOf(comb));
         
         for (int i = 0; i < others.length(); i++) {
-            recursive(comb + others.charAt(i), others.substring(0, i) + others.substring(i+1));
+            recursive(comb + others.charAt(i), others.substring(0,i) + others.substring(i+1));
         }
     }
     
-    private static boolean isPrime(int index) {
-        if (index == 0 || index == 1) return false;
-        
+    static private boolean isPrime(int index) {
+        if (index == 1 || index == 0) return false;
         int lim = (int)Math.sqrt(index);
-        for (int i = 2; i <= lim; i++) {
-            if (index % i == 0) return false;
+        
+        for (int i = 2; i <= lim ; i++) {
+            if (index % i == 0 ) return false;
         }
+        
         return true;
     }
     
     public int solution(String numbers) {
-    
         recursive("", numbers);
         
-        Iterator<Integer> it = numbersSet.iterator();
         int answer = 0;
         
-        while(it.hasNext()){
-            if (isPrime(it.next())) answer++;
+        for (Integer a : set) {
+            if (isPrime(a)) answer++;
         }
-        
         return answer;
     }
 }
