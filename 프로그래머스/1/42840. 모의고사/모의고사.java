bc4e36a5745435189935d42073700d04f2fe9fc1
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-06-22 20:34:07 +0900

    [level 1] Title: 모의고사, Time: 0.95 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,25 +1,26 @@
+// 8:18 ~ 8:50
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public ArrayList<Integer> solution(int[] answers) {
         int[] one = {1,2,3,4,5};
         int[] two = {2,1,2,3,2,4,2,5};
         int[] three = {3,3,1,1,2,2,4,4,5,5};
-        int[] results = new int[3];
+        int[] result = new int[3];
         
-        for (int i = 0; i < answers.length; i++) {
-            if (one[i % 5] == answers[i]) results[0]++;
-            if (two[i % 8] == answers[i]) results[1]++;
-            if (three[i % 10] == answers[i]) results[2]++;
+        int cnt = 0;
+        for (int a : answers) {
+            if (one[cnt % 5] == a) result[0]++;
+            if (two[cnt % 8] == a) result[1]++;
+            if (three[cnt % 10] == a) result[2]++;
+            cnt++;
         }
         
-        int max = Math.max(Math.max(results[0],results[1]), results[2]);
+        int max = Math.max(Math.max(result[0], result[1]), result[2]);
         
         ArrayList<Integer> list = new ArrayList<>();
-        
-        for (int i =0; i < results.length; i++) {
-            if (results[i] == max) list.add(i+1);
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] == max) list.add(i+1);
         }
         return list;
     }
