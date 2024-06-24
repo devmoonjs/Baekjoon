bc3d03e0bfce1617569f366b48fc7bdb28cffe1d
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-06-24 13:33:22 +0900

    [level 2] Title: 모음 사전, Time: 25.00 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,35 +1,29 @@
-// 알파벳 조합으로 사전을 만든다. 
-// 길이는 5를 넘지 않으며,
-// 주어진 단어의 인덱스 값을 return 하자.
-// 조합 -> 재귀 dfs 활용
+// dfs()
 import java.util.*;
 
 class Solution {
     
     static Set<String> set = new LinkedHashSet<>();
-    static String str; 
     
-    private static void dfs(String comb, String str) {
-        if (comb.length() == 6) return;
-        if (comb != "") set.add(comb);
-        
-        for (int i = 0; i < str.length(); i++) {
-            dfs(comb + str.charAt(i), str);
+    
+    private static void dfs(String str, String words) {
+        if (str.length() == 6) return;
+        if (!str.isEmpty()) set.add(str);
+        for (int i = 0; i < 5; i++) {
+            dfs(str + words.charAt(i), words);
         }
     }
-    
     public int solution(String word) {
-        str = "AEIOU";
-        dfs("", str);
+        String words = "AEIOU";
+        dfs("", words);
         
         Iterator<String> it = set.iterator();
-        
-        int index = 0;
+        int cnt = 0;
         while(it.hasNext()) {
-            index++;
+            cnt += 1;
             if (it.next().equals(word)) break;
         }
         
-        return index;
+        return cnt;
     }
 }
