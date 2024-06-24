6c7005f87bfb9d4ad689e888cad3d73a6be022fe
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-06-24 15:09:25 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 15800 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,62 +1,62 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static final int MAX = 60;
-    static int M, N;
-    static int[][] graph;
-    static boolean[][] visited;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+	
+	static int[][] arr;
+	static boolean[][] v;
+	static int[] dx = {1, 0, -1, 0};
+	static int[] dy = {0, 1, 0, -1};
+	static int n;
+	static int m;
+	
+	private static void dfs(int x, int y) {
+		v[x][y] = true;
+		
+		for (int i = 0; i < 4; i++) {
+			int newX = x + dx[i];
+			int newY = y + dy[i];
+			
+			if (newX >= 0 && newX < m && newY >= 0 && newY < n) {
+				if (arr[newX][newY] != 0 && !v[newX][newY]) {
+					dfs(newX, newY);
+				}
+			}
+		}
+	}
 
-    private static void dfs(int x, int y) {
-        visited[x][y] = true;
-        for (int i = 0; i < 4; i++) {
-            int newX = x + dx[i];
-            int newY = y + dy[i];
-            if (graph[newX][newY] == 1 && !visited[newX][newY]) {
-                dfs(newX, newY);
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-
-        while (T-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            M = Integer.parseInt(st.nextToken()); // 가로 길이
-            N = Integer.parseInt(st.nextToken()); // 세로 길이
-            int K = Integer.parseInt(st.nextToken());
-
-            graph = new int[MAX][MAX];
-            visited = new boolean[MAX][MAX];
-
-            // 그래프 정보 입력
-            for (int i = 0; i < K; i ++) {
-                st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken()); // 가로
-                int y = Integer.parseInt(st.nextToken()); // 세로
-
-                graph[y+1][x+1] = 1;
-
-            }
-
-            int answer = 0;
-            for (int i = 1; i < N+1; i++) {
-                for (int j = 1; j < M+1; j++) {
-                    if (graph[i][j] == 1 && !visited[i][j]) {
-                        dfs(i, j);
-                        answer += 1;
-                    }
-                }
-            }
-            System.out.println(answer);
-        }
-    }
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		while (t-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			m = Integer.parseInt(st.nextToken());
+			n = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			
+			arr = new int[m][n];
+			v = new boolean[m][n];
+			
+			// 그래프 생성
+			for (int i = 0; i < k; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+	
+				arr[a][b] = 1;
+			}
+			
+			int dfsCnt = 0;
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					if (arr[i][j] != 0 && !v[i][j]) {
+						dfs(i,j);
+						dfsCnt++;
+					}
+				}
+			}
+			System.out.println(dfsCnt);
+		}
+	}
 }
-
