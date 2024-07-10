8472541f40f71e69df09f19f7ad6374b94a5bda5
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-07-10 14:30:38 +0900

    [level 1] Title: 약수의 합, Time: 0.09 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,13 +1,11 @@
 class Solution {
     public int solution(int n) {
-        
-        int sum = 0;
-        
+        int answer = 0;
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
-                sum += i;        
+                answer += (n / i);
             }
         }
-        return sum;
+        return answer;
     }
 }
