b2ccd60594e75ca54803ed2c8694c7b39ee0fcb8
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-07-26 16:05:23 +0900

    [level 1] Title: 하샤드 수, Time: 0.06 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,17 +1,20 @@
+// 양의 정수 X 의 모든 자릿수의 합을 sum 이라 가정!
+// X % sum == 0 인 경우 하샤드 수라고 판별
+
 class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
-        
-        String str = Integer.toString(x);
-        
         int sum = 0;
+        int temp = x;
         
-        for (int i = 0; i < str.length(); i++) {
-            sum += str.charAt(i) - '0';    
+        while(x > 0) {
+            sum += x % 10;
+            x /= 10;
         }
         
-        if (x % sum != 0) answer = false;
-        
-        return answer;
+        if (temp % sum == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
