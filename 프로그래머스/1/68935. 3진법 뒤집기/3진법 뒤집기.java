85e9d9b2631fd7edd894e35673f610e60630d7d2
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-07-31 09:51:04 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,29 @@
+// 45 % 3 = 0
+// 15 % 3 = 0
+// 5 % 3 = 2
+// 1 % 3 == 1
+
+class Solution {
+    public int solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        
+        while(n > 0) {
+            if (n / 3 != 0) {
+                sb.append(n % 3);
+                n /= 3;
+            } else {
+                sb.append(n % 3);
+                break;    
+            }
+        }
+        String first = sb.toString();
+        int sum = 0;
+        int temp = 1;
+        for (int i = first.length() - 1; i >= 0; i--) {
+            sum += (first.charAt(i) - '0') * temp;
+            temp *= 3;
+        }
+        return sum;
+    }
+}
