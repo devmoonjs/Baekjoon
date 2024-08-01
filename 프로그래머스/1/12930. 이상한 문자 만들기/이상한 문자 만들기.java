1480c907dd5b31378e42c1e189ecd8d4beaf0865
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-08-01 09:44:45 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 1.22 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,27 @@
+//짝수번째 알파벳은 대문자
+// 홀수번째 알파벳은 소문자로
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        char[] arr = s.toCharArray();
+        
+        int cnt = 0;
+        for (int i = 0; i < arr.length; i++) { // 일단 문자열 char 배열에 저장
+            if (arr[i] == ' '){ // 공백 체크
+                cnt = 0;
+                continue;
+            }
+            
+            if (cnt % 2 == 0 && Character.isLowerCase(arr[i])) { // 짝수고 소문자면
+                arr[i] = Character.toUpperCase(arr[i]);
+                
+            } else if (cnt % 2 != 0 && Character.isUpperCase(arr[i])) { // 홀수고 대문자면
+                arr[i] = Character.toLowerCase(arr[i]);
+            }
+            cnt++;
+        }
+        answer = String.copyValueOf(arr);
+        return answer;
+    }
+}
