aaa3cbed0c719fc1ac317d49fa25f2106a769b85
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-08-08 16:53:37 +0900

    [level 1] Title: 삼총사, Time: 0.07 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,14 +1,15 @@
-class Solution {
+class Solution { 
     
     int cnt = 0;
     
-    void dfs(int[] number, int start, int depth, int sum) {
-        if (depth == 3) {
+    void dfs(int[] number, int start, int depth, int sum) { // 배열, 시작위치, 깊이, 누적합
+        if (depth == 3) { // 깊이가 3일때, 누적합이 0이면 cnt++
             if (sum == 0) {
                 cnt++;
             }
             return;
         }
+        
         for (int i = start; i < number.length; i++) {
             dfs(number, i + 1, depth + 1, sum + number[i]);
         }
