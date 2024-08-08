5adedc76232b9f2262adf0776c84b7dde45c3e54
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-08-08 16:41:46 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 3.00 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,3 +1,7 @@
+// n 을 하나하나 잘라서 활용! (String 형으로 바꾸는 것이 좋겠다)
+// 각 자릿수 별로 자른 값을 배열에 넣은 뒤 -> 해당 배열을 오름차순 정렬하고 -> 뒤에서 부터 읽으면 최댓값
+// 정렬 : Array.sort 활용
+// 새로운 정수 만드는 과정 StringBuilder 활용
 import java.util.*;
 
 class Solution {
