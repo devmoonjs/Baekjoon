374a1927edbd1f183d86a3d25808c974b32febef
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-08-14 00:15:52 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 3.34 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,11 +1,4 @@
-// 스택?
-// 칼로리가 낮은 음식을 먼저 먹을 수 있게
-// 음식의 개수가 2의 배수이면 그냥 넣고, 안된다면 몫의 개수만큼만 넣는다. 
-// 다 넣으면 0을 출력
-// 그리고 이제 다시 빼기
-
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public String solution(int[] food) {
