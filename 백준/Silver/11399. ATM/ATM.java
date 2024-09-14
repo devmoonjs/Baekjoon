5ec2351b7d76db3f68f1ad47df1bd0c29fe83d57
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-09-14 23:33:34 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14464 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,31 +1,31 @@
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
-
 public class Main {
-	
-	public static void main (String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(arr);
-		int sum = 0;
-		int result = 0;
-		
-		for (int i = 0; i < N; i++) {
-			sum += arr[i];
-			result += sum;
-		}
-		System.out.println(result);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        Integer sum = 0;
+        Integer result = 0;
+
+        for (Integer temp : arr) {
+            sum += temp;
+            result += sum;
+        }
+        System.out.println(result);
+    }
 }
