b2d4284fb9cd8ad2935c4cbbfbec07232a623ff3
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-12-06 16:31:50 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 27.86 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 71.9 MB, 시간: 0.11 ms
+메모리: 96.3 MB, 시간: 27.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 06일 19:25:14
+2024년 12월 06일 16:31:44
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,36 +1,44 @@
+import java.io.*;
 import java.util.*;
+
 class Solution {
+    /**
+    * 들어갈 때 + 1
+    * 나갈 때 + 1
+    * 1. bridge 큐 생성
+    *    int nowWeight = 0; 생성 (다리에 올라간 트럭 무게)
+    * 2. bridge 큐 크기가 bridge_length 보다 작으면, truck 배열에서 하나 추가 (time++, nW 추가)
+    * 3. 만약, 큐 내의 트럭 무게 총합이 weight 보다 작다면 더 추가 (time++, nW 추가)
+    * 4. 그렇지 않다면, poll() (time++, nW 감소)
+    */
+    
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        
         Queue<Integer> bridge = new LinkedList<>();
         int sum = 0;
         int time = 0;
 
-        for (int i = 0; i < truck_weights.length; i++) {
-            int truck = truck_weights[i];
-
+        for (Integer truck : truck_weights) {
             while (true) {
-                if (bridge.isEmpty()) {
+                time++;
+
+                // 다리가 찼을 경우
+                if (bridge.size() == bridge_length) {
+                    sum -= bridge.poll();
+                }
+
+                // 트럭이 추가 가능하다면
+                if (sum + truck <= weight) {
                     bridge.add(truck);
                     sum += truck;
-                    time++;
                     break;
-                } else if (bridge.size() == bridge_length) { // 다리에 트럭이 전부 찬 경우
-                    sum -= bridge.poll();
-                } else { // 다리에 트럭 자리가 남았을 때
-                    if (sum + truck <= weight) {
-                        bridge.add(truck);
-                        sum += truck;
-                        time++;
-                        break;
-                    } else {
-                        bridge.add(0);
-                        time++;
-                    }
+                }
+                // 자리는 있지만, 더 이상 들어갈 수 없을 때
+                else {
+                    // 4. 트럭을 추가할 수 없으면 자리는 비워 둠
+                    bridge.add(0); // 이 부분 생략 가능
                 }
             }
         }
-        int result =time + bridge_length;
-        return result;
+        return time + bridge_length;
     }
 }
