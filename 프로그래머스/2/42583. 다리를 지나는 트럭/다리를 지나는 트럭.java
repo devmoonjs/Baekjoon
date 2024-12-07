3e9a1d4a39c96d9311a6821be9f20d0604d4a30e
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-12-07 23:47:00 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 25.12 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,15 +2,6 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    /**
-    * 들어갈 때 + 1
-    * 나갈 때 + 1
-    * 1. bridge 큐 생성
-    *    int nowWeight = 0; 생성 (다리에 올라간 트럭 무게)
-    * 2. bridge 큐 크기가 bridge_length 보다 작으면, truck 배열에서 하나 추가 (time++, nW 추가)
-    * 3. 만약, 큐 내의 트럭 무게 총합이 weight 보다 작다면 더 추가 (time++, nW 추가)
-    * 4. 그렇지 않다면, poll() (time++, nW 감소)
-    */
     
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         Queue<Integer> bridge = new LinkedList<>();
