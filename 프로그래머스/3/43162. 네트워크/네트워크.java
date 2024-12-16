a00c9dacee875723f595dcc7a7da9d51f9fd580e
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-12-16 19:12:39 +0900

    [level 3] Title: 네트워크, Time: 0.64 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,24 +3,24 @@ class Solution {
     static boolean[] visited;
     static int[][] computers;
     
-    private static void dfs(int index) {
+    static void dfs(int index) {
         visited[index] = true;
         
         for (int i = 0; i < computers[index].length; i++) {
-            if (computers[index][i] == 1 & !visited[i]) {
-                dfs(i);
-            }
+            if (computers[index][i] == 1 && !visited[i])
+            dfs(i);
         }
-        
     }
     
+    
     public int solution(int n, int[][] computers) {
-        visited = new boolean[n];
         this.computers = computers;
+        visited = new boolean[n];
         
         int cnt = 0;
+        
         for (int i = 0; i < computers.length; i++) {
-            if (!visited[i]){
+            if (!visited[i]) {
                 dfs(i);
                 cnt++;
             }
