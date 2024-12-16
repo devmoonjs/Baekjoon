124499137ca810135131acea14f4c6d6177b9a11
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-12-16 17:37:14 +0900

    [level 2] Title: 타겟 넘버, Time: 8.18 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,30 +1,29 @@
 class Solution {
     
     static int[] numbers;
+    static int cnt = 0;
     static int target;
-    static int len;
-    static int answer;
     
-    static void dfs(int index, int sum) {
-        if (index == len) {
+    static void comb(int index, int sum) {
+        if (index == numbers.length) {
             if (sum == target) {
-                answer++;
+                cnt++;
             }
+            return;
         }
         
-        if (index < len) {
-            dfs(index+1, sum + numbers[index]);
-            dfs(index+1, sum - numbers[index]);
+        if (index < numbers.length) {
+            comb(index+1, sum + numbers[index]);
+            comb(index+1, sum - numbers[index]);
         }
     }
     
     public int solution(int[] numbers, int target) {
         this.numbers = numbers;
         this.target = target;
-        this.len = numbers.length;
         
-        dfs(0,0);
+        comb(0,0);
         
-        return answer;
+        return cnt;
     }
 }
