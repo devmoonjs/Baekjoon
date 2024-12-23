bb29d41a8719a351797511502eb5b4daaf8efd33
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-12-23 17:38:33 +0900

    [level 3] Title: 섬 연결하기, Time: 0.80 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] costs) {
+        // union 배열 초기화
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        
+        // 비용 정렬
+        Arrays.sort(costs, (a, b) -> Integer.compare(a[2], b[2]));
+        
+        int totalCost = 0;
+        for (int[] cost : costs) {
+            int from = cost[0];
+            int to = cost[1];
+            int bridgeCost = cost[2];
+            
+            if (find(parent, from) != find(parent, to)) {
+                union(parent, from, to);
+                totalCost += bridgeCost;
+            }
+        }
+        return totalCost;
+    }
+    
+    private int find(int[] parent, int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent, parent[x]);
+    }
+    
+    private void union(int[] parent, int x, int y) {
+        int rootX = find(parent, x);
+        int rootY = find(parent, y);
+        
+        if (rootX != rootY) {
+            parent[rootY] = rootX;
+        }
+    }
+}
