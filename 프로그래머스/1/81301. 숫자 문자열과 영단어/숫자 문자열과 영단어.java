b82e449a031736df94c1eadc7d67e12e62fc900d
Moon <130039001+devmoonjs@users.noreply.github.com>
2024-12-31 11:53:28 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.12 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,15 +1,9 @@
-// 숫자가 나오기 직전까지 sb.append()
-// map.get(sb) 해서 밸류값 찾기 그리고 sb2에 담기
-// 숫자가 나오면 continue;
-
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int solution(String s) {
-        StringBuilder sb = new StringBuilder();
         Map<String, Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-        
         map.put("zero", 0);
         map.put("one", 1);
         map.put("two", 2);
@@ -21,19 +15,25 @@ class Solution {
         map.put("eight", 8);
         map.put("nine", 9);
         
-        StringBuilder result = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
         
         for (int i = 0; i < s.length(); i++) {
-            if (Character.isDigit(s.charAt(i))) { // 숫자라면
-                result.append(s.charAt(i));
-            } else{
-                sb.append(s.charAt(i));
-                if (map.containsKey(sb.toString())) {
-                    result.append(map.get(sb.toString()));
-                    sb = new StringBuilder();
+            char now = s.charAt(i);
+            
+            if (Character.isDigit(now)) {
+                answer.append(now);
+                
+            } else {
+                temp.append(now);
+                
+                if (map.containsKey(temp.toString())) {
+                    answer.append(map.get(temp.toString()));
+                    
+                    temp = new StringBuilder();
                 }
             }
         }
-        return Integer.parseInt(result.toString());
+        return Integer.parseInt(answer.toString());
     }
 }
