0150e9d73706cbe0245b51728deabfe2989dcafd
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-01-17 00:44:23 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 46.68 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,44 +1,48 @@
-/*
-    13:49
-    현재 난이도 : diff
-    현재 퍼즐 소요시간 : time_cur
-    이전 퍼즐 소요시간 : time_prev
-    나의 숙련도 : level
-    
-    퍼즐 틀릴 때 -> (time_cur+time_prev) * (diff-level) + time_cur
-*/
-
 import java.util.*;
 
 class Solution {
+    
+    static int[] diffs;
+    static int[] times;
+    
+    public static long calTime(int level) {
+        long spentTime = 0;
+        long timePrev = 0;
+        
+        for (int i = 0 ; i < diffs.length; i++) {
+            if (level < diffs[i]) {
+                spentTime += (timePrev + times[i]) * (diffs[i] - level) + times[i];
+                timePrev = times[i];
+                
+            } else {
+                spentTime += times[i];
+                timePrev = times[i];
+            }
+        }
+        
+        return spentTime;
+    }
+    
     public int solution(int[] diffs, int[] times, long limit) {
+        this.diffs = diffs;
+        this.times = times;
+        
         int left = Arrays.stream(diffs).min().orElseThrow();
         int right = Arrays.stream(diffs).max().orElseThrow();
-        
         
         int mid = 0;
         
         ArrayList<Integer> answers = new ArrayList<>();
+        
         int diffsLen = diffs.length;
         
         while (left <= right) {
-            long spentTime = 0;
             mid = (left + right) / 2;
-            long timePrev = 0;
-            
-            for (int i = 0; i < diffsLen; i++) {
-                if (diffs[i] >  mid) {
-                    spentTime = spentTime + times[i] + 
-                        (timePrev + times[i]) * (diffs[i] - mid);
-                    timePrev = times[i];
-                } else {
-                    spentTime += times[i];
-                    timePrev = times[i];
-                }
-            }
+            long spentTime = calTime(mid);
             
             if (spentTime > limit) {
                 left = mid + 1;
+                
             } else {
                 right = mid - 1;
                 answers.add(mid);
