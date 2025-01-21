a6f67317c7c22b52eed06835aab3f60bf7903587
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-01-21 15:01:04 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,20 +1,7 @@
-/*
-    11:41
-    다음 순서 -> 몫 + 나머지
-    따라서, 몫 + 나머지가 같아질 때 종료 return
-    
-    while (true) {
-        a = a / 2 + a % 2;
-        b = b / 2 + b % 2;
-        cnt++;
-        if (a == b) false;
-    }
-    return cnt;
-*/
-
 class Solution
 {
-    public int solution(int n, int a, int b) {
+    public int solution(int n, int a, int b)
+    {
         int answer = 0;
         
         while(true) {
@@ -24,7 +11,7 @@ class Solution
             
             if (a == b) break;
         }
-
+        
         return answer;
     }
 }
