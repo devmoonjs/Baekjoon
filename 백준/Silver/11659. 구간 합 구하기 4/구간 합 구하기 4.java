137f83b0f3d3015a3edebb75da9ef70855a2483b
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-01-22 12:07:40 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1124 ms, Memory: 59420 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,29 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+/*
+    i ~ j 번째 수까지 합을 구하는 프로그램
+*/
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        long[] S = new long[N + 1];
+        int N = Integer.parseInt(st.nextToken()); // 수의 개수
+        int M = Integer.parseInt(st.nextToken()); // 케이스 수
+        long[] numbers = new long[N + 1];
 
-        st = new StringTokenizer(bf.readLine());
+        st = new StringTokenizer(br.readLine());
 
-        for (int i = 1  ; i < N + 1; i++) {
-            S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
+        for (int i = 1; i < N + 1; i++) {
+            numbers[i] = numbers[i - 1] + Integer.parseInt(st.nextToken());
         }
 
-        for (int j = 0; j < M; j++) {
-            st = new StringTokenizer(bf.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            System.out.println(S[b] - S[a-1]);
+        for (int testCase = 0; testCase < M; testCase++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
 
+            System.out.println(numbers[j] - numbers[i-1]);
         }
     }
 }
