8d2b4e53ff6871ca9227dddfd42e48080666b265
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-02-13 19:55:08 +0900

    [level 2] Title: 괄호 회전하기, Time: 54.86 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,12 +1,37 @@
+/*
+    for (s 길이만큼) {
+        //회전
+        String rotated = s.substring(i) + s.substring(0,i);
+        
+        // 올바른 괄호인지 체크
+        if (isValid(rotated)) {
+            answer++;
+        }
+    }
+    
+    private static boolean isValid(String rotated) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : rotated.toCharArray()) {
+            if (stack이 비어있으면) stack.push(c);
+            char top = stack.peek();
+            if (c 가 여는 괄호 이면) stack.push(c);
+            if (c 가 닫는 괄호이고 top 이 여는 괄호이면) stack.pop();
+        }
+        return stack.isEmpty();
+    }
+*/
 import java.util.*;
 
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        int sLen = s.length();
         
-        for (int i = 0; i < s.length(); i++) {
-            String rotated = s.substring(i) + s.substring(0, i);
-            if (isValid(rotated)) answer++;
+        for (int i = 0; i < sLen; i++) {
+            String rotated = s.substring(i) + s.substring(0,i);
+            if (isValid(rotated)) {
+                answer++;
+            }
         }
         return answer;
     }
@@ -15,16 +40,17 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         
         for (char c : rotated.toCharArray()) {
-            if (c == '[' || c == '{' || c == '(') stack.push(c);
-            else {
-                if (stack.isEmpty()) return false;
-                
-                char top = stack.peek();
-                if (c == ']' &&  top == '['
-                    || c == '}' && top == '{'
-                    || c == ')' && top == '(') {
-                    stack.pop();
-                }
+            if (stack.isEmpty())  {
+                stack.push(c);
+                continue;
+            }
+            char top = stack.peek();
+            
+            if (c == '(' || c == '{' || c == '[') stack.push(c);
+            if (c == ')' && top == '(' 
+               || c == '}' && top == '{'
+               || c == ']' && top == '[') {
+                stack.pop();
             }
         }
         return stack.isEmpty();
