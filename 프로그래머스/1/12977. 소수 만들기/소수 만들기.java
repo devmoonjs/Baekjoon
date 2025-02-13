e7f6328bbc9d720b7ecc1f16c7581d837554598a
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-02-13 20:00:52 +0900

    [level 1] Title: 소수 만들기, Time: 12.89 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,28 +1,24 @@
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
         int numsLen = nums.length;
         int sum = 0;
-        
+        int answer = 0;
         for (int i = 0; i < numsLen - 2; i++) {
             for (int j = i+1; j < numsLen - 1; j++) {
                 for (int k = j+1; k < numsLen; k++) {
                     sum = nums[i] + nums[j] + nums[k];
-                    if (isPrime(sum)) {
-                        answer++;
-                    }
+                    if (isPrime(sum)) answer++;
                 }
             }
         }
         return answer;
     }
     
-    private static boolean isPrime(int a) {
-        if (a < 2) return false;
-        for (int i = 2; i < a; i++) {
-            if (a % i == 0) return false;
+    private static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) return false;
         }
-        
         return true;
     }
 }
