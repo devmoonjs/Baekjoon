9d57b63137be3fa3a21f8da5dbd6cc95050fae0e
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-02-15 00:53:22 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 108 ms, Memory: 15428 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,58 +1,62 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int[] number;
-    static int[] operator;
+    static int min;
+    static int max;
     static int N;
-    static int MAX = Integer.MIN_VALUE;
-    static int MIN = Integer.MAX_VALUE;
+    static int[] key;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-
-        number = new int[N];
-        operator = new int[4];
+        arr = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < N; i++) {
-            number[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        key = new int[4];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 4; i++) {
-            operator[i] = Integer.parseInt(st.nextToken());
+        for (int i =0; i < key.length; i++) {
+            key[i] = Integer.parseInt(st.nextToken());
         }
 
-        dfs(number[0], 1);
+        min = Integer.MAX_VALUE;
+        max = Integer.MIN_VALUE;
 
-        System.out.println(MAX);
-        System.out.println(MIN);
+        dfs(arr[0], 1);
+
+        System.out.println(max);
+        System.out.println(min);
     }
 
-    public static void dfs(int num, int idx) {
+    private static void dfs(int num, int idx) {
         if (idx == N) {
-            MAX = Math.max(MAX, num);
-            MIN = Math.min(MIN, num);
+            max = Math.max(max, num);
+            min = Math.min(min, num);
             return;
         }
 
         for (int i = 0; i < 4; i++) {
-            if (operator[i] > 0) {
-                operator[i]--;
+            if (key[i] > 0) {
+                key[i]--;
 
                 switch (i) {
-                    case 0: dfs(num + number[idx], idx + 1); break;
-                    case 1: dfs(num - number[idx], idx + 1); break;
-                    case 2: dfs(num * number[idx], idx + 1); break;
-                    case 3: dfs(num / number[idx], idx + 1); break;
+                    case 0: dfs(num + arr[idx], idx + 1); break;
+                    case 1: dfs(num - arr[idx], idx + 1); break;
+                    case 2: dfs(num * arr[idx], idx + 1); break;
+                    case 3: dfs(num / arr[idx], idx + 1); break;
+                
                 }
-                operator[i]++;
+
+                key[i]++;
             }
         }
-     }
+    }
 }
