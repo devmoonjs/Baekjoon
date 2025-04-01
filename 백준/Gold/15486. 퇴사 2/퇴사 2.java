82f5a649b0ed3d2962c2a67298b36ae28a2e858e
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-04-01 17:31:56 +0900

    [Gold V] Title: 퇴사 2, Time: 756 ms, Memory: 339244 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,38 +1,36 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N;
-	static int[][] data;
 
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		
-		data = new int [N+1][2];
-		for (int i = 1; i <= N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			data[i][0] = Integer.parseInt(st.nextToken());
-			data[i][1] = Integer.parseInt(st.nextToken());
-		}
-		
-		System.out.println(dp());
-		
-	}
-	
-	private static int dp() {
-		int[] dp = new int[N+2];
-		
-		for (int i = 1; i <= N; i++) {
-			// 오늘 작업의 결과 반영 (상담 끝난 다음날 저장됨)
-			int next = i + data[i][0];
-			if (next <= N +1) {
-				dp[next] = Math.max(dp[next], dp[i] + data[i][1]);
-			}
-			
-			dp[i+1] = Math.max(dp[i+1], dp[i]);
-		}
-		return dp[N+1];
-	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N+1][2];
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int day = Integer.parseInt(st.nextToken());
+            int pay = Integer.parseInt(st.nextToken());
+            arr[i][0] = day;
+            arr[i][1] = pay;
+        }
+
+        int[] dp = new int[N+2];
+
+        for (int i = 1; i <= N; i++) {
+            int nowDay = arr[i][0];
+            int nowPay = arr[i][1];
+
+            if (nowDay + i <= N+1) {
+                dp[nowDay + i] = Math.max(dp[nowDay + i], dp[i] + nowPay);
+            }
+            dp[i+1] = Math.max(dp[i+1], dp[i]);
+        }
+        
+        System.out.println(dp[N+1]);
+    }
 }
