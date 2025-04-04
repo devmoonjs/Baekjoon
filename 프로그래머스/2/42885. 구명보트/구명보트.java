7fcd8dea950232319a23232ecf0cf01ba9559aa6
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-04-04 23:46:04 +0900

    [level 2] Title: 구명보트, Time: 14.40 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,15 +1,33 @@
 import java.util.*;
 
+/*
+    1. 내림차순 정렬
+    2. while()
+*/
+
 class Solution {
     public int solution(int[] people, int limit) {
-        int ans = 0;
         Arrays.sort(people);
-        int min = 0;
+        int peopleCnt = people.length;
+        int boatCnt = 0;
+        int livePeople = 0;
+        int maxIndex = peopleCnt - 1;
+        int minIndex = 0;
         
-        for (int max = people.length - 1; min <= max; max--) {
-            if (people[min] + people[max] <= limit) min++;
-            ans++;
+        while(livePeople < peopleCnt) {
+            // 무조건 제일 Max 한 명 바로 탑승
+            int nowWeight = people[maxIndex];
+            livePeople++;
+            
+            // 두명 탑승 가능할 때
+            if (nowWeight + people[minIndex] <= limit) {
+                minIndex++;
+                livePeople++;
+            }
+            
+            maxIndex--;
+            boatCnt++;
         }
-        return ans;
+        return boatCnt;
     }
 }
