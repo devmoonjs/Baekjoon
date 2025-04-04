a7d068df319a25992fa4e54ff1be22bfe34cb968
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-04-04 22:57:25 +0900

    [level 2] Title: 튜플, Time: 61.30 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,25 +1,31 @@
 import java.util.*;
-
+/*
+    1. 길이별로 정렬
+    2. 숫자만 넣을 때, 없던 숫자면 넣기
+*/
 class Solution {
     public List<Integer> solution(String s) {
-        
-        s = s.substring(2, s.length() -2).replace("},{", "-");
+        s = s.substring(2, s.length() - 2).replace("},{", "-");
         String[] sArr = s.split("-");
         
-        Arrays.sort(sArr, Comparator.comparingInt(String::length));
-        List<Integer> list = new ArrayList<>();
+        Arrays.sort(sArr, new Comparator<>() {
+            @Override
+            public int compare(String a, String b) {
+                return a.length() - b.length();
+            }
+        });
         
+        List<Integer> answer = new ArrayList<>();
         for (String str : sArr) {
-            String[] check = str.split(",");
+            String[] tempArr = str.split(",");
             
-            for (int i = 0; i < check.length; i++) {
-                int num = Integer.parseInt(check[i]);
-                
-                if (!list.contains(num)) {
-                    list.add(num);
+            for (int i = 0; i < tempArr.length; i++) {
+                int num = Integer.parseInt(tempArr[i]);
+                if (!answer.contains(num)) {
+                    answer.add(num);
                 }
             }
         }
-        return list;
+        return answer;
     }
 }
