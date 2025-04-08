470206b919586451e2c7492f94eca5bae2a18014
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-04-08 15:19:33 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 820.18 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,12 +1,11 @@
-// 16:22
 class Solution {
     public long solution(int w, int h) {
-        long answer = 0;
         long W = (long)w;
         long H = (long)h;
+        long answer = 0;
         
-        for (long i = 1; i < W; i++) {
-            answer += H*i / W;    
+        for (int i = 1; i < w; i++) {
+            answer += H * i / W;
         }
         
         return answer * 2;
