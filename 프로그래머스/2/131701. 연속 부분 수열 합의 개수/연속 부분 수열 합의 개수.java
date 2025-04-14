c9aa9185e8903ba1eb9757f315afe88c92bf20b0
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-04-14 13:09:20 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 460.03 ms, Memory: 193 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,26 +1,22 @@
 /*
-    원형 배열
-    1개씩 더할 떄, -> 5번 j ~ i+j
-    2개씩 더할 때, -> 5번
-    3개씩 더할 때, -> 5번
-    4개씩 더할 때, -> 5번
-    5개씩 더할 때, -> 5번
-
+    원형 배열 -> 배열 길이 * 2
+    HashSet<Integer> set = new HashSet<>();
 */
 import java.util.*;
 
 class Solution {
     public int solution(int[] elements) {
-        int answer = 0;
-        int eleLen = elements.length;
+        int numberLen = elements.length;
+        int[] newArr = new int[numberLen* 2];
         HashSet<Integer> set = new HashSet<>();
         
-        for (int i = 1; i <= eleLen; i++) {
-            for (int j = 0; j < eleLen; j++) {
-                int sum = 0;
-                for (int k = j; k < j + i; k++) {
-                    sum += elements[k % eleLen];
-                }
+        for (int i = 0; i < numberLen; i++) {
+            newArr[i] = newArr[i+numberLen] = elements[i];
+        }
+        
+        for (int i = 1; i <= numberLen; i++) {
+            for (int j = 0; j < numberLen; j++) {
+                int sum = Arrays.stream(newArr, j, j + i - 1).sum();
                 set.add(sum);
             }
         }
