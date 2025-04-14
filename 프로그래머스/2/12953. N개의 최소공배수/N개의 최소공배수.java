424fbb20427f7abde98248b77cf3e8a19e3789fd
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-04-14 15:55:12 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,29 +1,22 @@
-/*
-    최소공배수 구하기
-*/
-
 class Solution {
     public int solution(int[] arr) {
-        int answer = 0;
+        int temp = arr[0];
         
-        int temp = lcm(arr[0], arr[1]);
-        
-        for (int i = 2; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             temp = lcm(temp, arr[i]);
         }
         
-        answer = temp;
-        return answer;
+        return temp;
     }
     
-    public int gcd(int a, int b) {
+    private int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+    
+    private int gcd(int a, int b) {
         if (b == 0) {
             return a;
         }
         return gcd(b, a % b);
-    }
-    
-    public int lcm(int a, int b) {
-        return (a * b) / gcd(a , b);
     }
 }
