b3bccad389cceffb60b3902e9c2f9397fcbb852b
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-04-14 14:32:47 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,20 +1,61 @@
+/*
+    int round = 1;
+    int nowPerson = 1;
+    for (words 전체 i) {
+        String preLastWord = words[i-1]
+        Char preLastChar = preLastWord.charAt(preLastWord.length()-1);
+        
+        String nowWord = words[i];
+        
+        if (nowWord.charAt(0) != preLastChar || wordList.contains(nowWord))
+        {
+            break;
+        }
+        
+        if (nowPerson == n) {
+            nowPerson = 1;
+            round++;
+        } else {
+            nowPerson++;
+        }
+    }
+*/
+
 import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        HashSet<String> used = new HashSet<>();
-        char lastChar = words[0].charAt(0);
+        HashSet<String> wordsSet = new HashSet<>();
+        wordsSet.add(words[0]);
         
-        for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            if (used.contains(word) || lastChar != word.charAt(0)) {
-                return new int[] {i % n + 1, i / n + 1};
+        int round = 1;
+        int nowPerson = 2;
+        boolean fail = false;
+        
+        for (int i = 1; i < words.length; i++) {
+            String preWord = words[i-1];
+            Character preLastChar = preWord.charAt(preWord.length() - 1);
+            
+            String nowWord = words[i];
+            
+            if (nowWord.charAt(0) != preLastChar || wordsSet.contains(nowWord)) {
+                fail = true;
+                break;
             }
             
-            used.add(word);
-            lastChar = word.charAt(word.length() - 1);
+            wordsSet.add(nowWord);
+            if (nowPerson == n) {
+                nowPerson = 1;
+                round++;
+            } else {
+                nowPerson++;
+            }
         }
         
-        return new int[] {0,0};
+        if (fail) {
+            return new int[]{nowPerson, round};
+        }
+        
+        return new int[]{0,0};
     }
 }
