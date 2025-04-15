210278742bef1db40681eef6be2d1c29fa521550
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-04-15 11:17:46 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 98.83 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,51 +1,46 @@
 import java.util.*;
 
-/*
-* q1 과 q2 의 합을 구하여 큰 쪽에서 원소를 빼 작은 쪽에 더하는 방식
-*/
-
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        
         int answer = -1;
         
-        Queue<Integer> q1 = new LinkedList<Integer>();
-        Queue<Integer> q2 = new LinkedList<Integer>();
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
         
         long sum1 = 0;
+        for (int a : queue1) {
+            q1.add(a);
+            sum1 += a;
+        }
+        
         long sum2 = 0;
-        
-        for(int t : queue1){
-            q1.add(t);
-            sum1 += t;
+        for (int b : queue2) {
+            q2.add(b);
+            sum2 += b;
         }
         
-        for(int t : queue2){
-            q2.add(t);
-            sum2 += t;
-        }
-
         int cnt = 0;
-        
-        while(cnt <= queue1.length * 4){
-            if(sum1 > sum2){
-                int poll = q1.poll();
-                sum1 -= poll;
-                sum2 += poll;
-                q2.add(poll);
+        while (cnt <= queue1.length * 4) {
+            if (sum1 > sum2) {
+                int temp = q1.poll();
+                sum1 -= temp;
+                sum2 += temp;
+                q2.add(temp);
                 
-            } else if(sum1 < sum2){
-                int poll = q2.poll();
-                sum2 -= poll;
-                sum1 += poll;
-                q1.add(poll);
+            } else if (sum1 < sum2) {
+                int temp = q2.poll();
+                sum1 += temp;
+                sum2 -= temp;
+                q1.add(temp);
                 
-            } else if(sum1 == sum2){
+            } else if (sum1 == sum2) {
                 answer = cnt;
                 break;
             }
+            
             cnt++;
         }
+
         return answer;
     }
 }
