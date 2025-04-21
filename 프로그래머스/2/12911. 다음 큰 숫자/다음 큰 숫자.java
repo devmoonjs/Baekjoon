33a94db2e48a3f5b3f4205417c6dede2ddb3fbf8
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-04-21 23:40:36 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -2,7 +2,6 @@ class Solution {
     static int i = 0;
     public int solution(int n) {
         int answer = 0;
-        
         int aBit = Integer.bitCount(n);
         
         for (i = n + 1; i <= 1000000; i++) {
