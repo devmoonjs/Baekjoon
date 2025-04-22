99b309bdd077d91c3f905a9851a813057c1ea5eb
Moon <130039001+devmoonjs@users.noreply.github.com>
2025-04-22 09:53:45 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -5,7 +5,7 @@ class Solution {
         dp[2] = 2;
         
         for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
         }
         
         return dp[n];
